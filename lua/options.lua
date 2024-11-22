4d4797f6057fa482f4b46a2a1bab6d8357143835
Facundo La Rocca <facu.larocca@gmail.com>
2024-11-22 09:51:42 -0300

    .

--- lua/options.lua
+++ lua/options.lua
@@ -49,7 +49,7 @@ opt.swapfile = false
 vim.lsp.inlay_hint.enable(true)
 
 vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldcolumn = 'auto:9' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
